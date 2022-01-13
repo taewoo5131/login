5cff2e76f6128a2e796820ba86d1a241522a16fc
leetaewoo <taewoo5131@gmail.com>
2022-01-13 12:46:55 +0900

    로그인 처리하기 - 쿠키 사용

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,42 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name="memberId",required = false) Long memberId , Model model) {
+        // valid
+        if (memberId == null) {
+            return "home";
+        }else{
+            Member loginMember = memberRepository.findById(memberId);
+            log.info("loginMember = {}" , loginMember);
+            // valid
+            if (loginMember == null) {
+                return "home";
+            }else{
+                // real login
+                model.addAttribute("member", loginMember);
+                return "loginHome";
+            }
+        }
     }
 }
